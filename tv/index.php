4a69250b82074b3b76567384650b4c0bc4cfa662
macca <mccsolo1942@gmail.com>
2023-04-30 00:58:06 +0200

    aggiunta visualizzazione modalità TV

---
+++ tv/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modalità TV - itiTV</title>
+    <meta name="description" content="INDEX CARICAMENTO PAGINA itiTV">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <img src="assets/images/logoiti.png" alt="LOGO ITI" width="200px">
+        </div>
+        <div class="main">
+            <h1>Benvenuti su</h1>
+            <center><img src="../admin/assets/images/logo.png" alt="LOGO ITI TV"></center>
+        </div>
+        <div class="footer">
+            <h1 id="ore"><?=date("H:i")?></h1>
+            <h1 id="data"><?=date("d/m/Y")?></h1>
+        </div>
+    </div>
+</body>
+</html>
