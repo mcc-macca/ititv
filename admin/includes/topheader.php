ac6f6fc155c4aaf7c614543b7fba5cc4a6384768
macca <mccsolo1942@gmail.com>
2023-04-30 00:28:53 +0200

    little mod

--- admin/includes/topheader.php
+++ admin/includes/topheader.php
@@ -14,36 +14,26 @@
                         <!-- Navbar-left -->
                         <ul class="nav navbar-nav navbar-left">
                             <li>
-                                <button class="button-menu-mobile open-left waves-effect">
+                                <button class="button-menu-mobile open-left waves-effect text-white">
                                     <i class="mdi mdi-menu"></i>
                                 </button>
                             </li>
-                     
-                    
                         </ul>
-
                         <!-- Right(Notification) -->
                         <ul class="nav navbar-nav navbar-right bg-dark text-white">
-                          
-
                             <li class="dropdown user-box">
                                 <a href="" class="dropdown-toggle waves-effect user-link" data-toggle="dropdown" aria-expanded="true">
                                     <img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
                                 </a>
-
                                 <ul class="dropdown-menu dropdown-menu-right arrow-dropdown-menu arrow-menu-right user-list notify-list">
                                     <li>
                                         <h5>Ciao <?=$_SESSION['login']?></h5>
                                     </li>
-                              
                                     <li><a href="change-password.php"><i class="ti-settings m-r-5"></i> Cambia password</a></li>
-                           
                                     <li><a href="logout.php"><i class="ti-power-off m-r-5"></i> Disconnettiti</a></li>
                                 </ul>
                             </li>
-
                         </ul> <!-- end navbar-right -->
-
                     </div><!-- end container -->
                 </div><!-- end navbar -->
             </div>
